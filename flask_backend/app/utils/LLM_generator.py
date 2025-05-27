d2bf681a6103109ad395c07d5726ce8456367607
Csaba [suvicsabika] <suvicsabika@gmail.com>
2025-05-27 18:54:45 +0200

    After an internal discussion: refactoring the repository and the project itself:
    Deleted several files, including placeholders and unused scripts, alongside redundant dependencies in `requirements.txt`. These changes aim to clean up the project structure and remove dead code for better maintainability.

--- flask_backend/app/utils/LLM_generator.py
+++
@@ -1,6 +0,0 @@
-# This util would generate the necessary data with LLM technology... TODO
-# If necessary, please create more folders/packages
-
-# We will implement the solution together HERE
-# We will implement the solution together HERE
-# We will implement the solution together HERE
